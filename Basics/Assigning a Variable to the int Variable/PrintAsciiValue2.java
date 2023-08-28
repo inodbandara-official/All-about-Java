f98b58576427763f9b08b4ce71ee9c39daa10b54
Inod Bandara <87967504+inodbandara-official@users.noreply.github.com>
2023-08-28 20:31:26 +0530

    Create PrintAsciiValue2.java

---
+++ Basics/Assigning a Variable to the int Variable/PrintAsciiValue2.java
@@ -0,0 +1,10 @@
+public class PrintAsciiValue2  
+{  
+public static void main(String[] String)   
+{  
+int ch1 = 'a';  
+int ch2 = 'b';  
+System.out.println("The ASCII value of a is: "+ch1);  
+System.out.println("The ASCII value of b is: "+ch2);  
+}  
+} 
